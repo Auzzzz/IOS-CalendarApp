5f659cac0c280a445e78e436d69ac04751951d55
Auzzzz <s3544232@student.rmit.edu.au>
2020-08-02 21:28:45 +1000

    Initial Commit

---
+++ CalendarApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalendarApp
+//
+//  Created by Chris Kelly on 2/8/20.
+//  Copyright © 2020 Chris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
